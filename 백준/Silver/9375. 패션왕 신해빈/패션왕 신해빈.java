0c60324727bc3fdb54e178c79c945b99e8081fef
ap_dui <104255115+SXXNHU@users.noreply.github.com>
2026-01-21 19:59:51 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 96 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            HashMap<String, Integer> map = new HashMap<>();
+            int n = Integer.parseInt(br.readLine());
+
+            while (n-- > 0) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                st.nextToken();
+                String kind = st.nextToken();
+
+                if (map.containsKey(kind)) {
+                    map.put(kind, map.get(kind) + 1);
+                } else {
+                    map.put(kind, 1);
+                }
+            }
+
+            int result = 1;
+
+            for (int val : map.values()) {
+                result *= (val + 1);
+            }
+            sb.append(result - 1).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
