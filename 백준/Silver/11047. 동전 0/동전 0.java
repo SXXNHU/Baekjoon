5cb3e2a5ba4a039afe31f5274085060b17602ba5
ap_dui <104255115+SXXNHU@users.noreply.github.com>
2026-01-21 19:28:18 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());  
+        int k = Integer.parseInt(st.nextToken());  
+        
+        int count = 0;
+        
+        int[] coins = new int[n];  
+        
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        
+        
+        for (int i = n-1; i >= 0; i--) {
+            if (k / coins[i] > 0) {
+                count += (k/coins[i]);
+                k -= (k/coins[i])*coins[i];
+            }
+        }
+        System.out.print(count);
+    }
+}
